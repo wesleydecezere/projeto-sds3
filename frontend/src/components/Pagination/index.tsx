7e64aab100f52f7effaf378ec8e6bbc02952136f
wesleydecezere <wesleydecezere@gmail.com>
2021-10-05 11:01:41 -0300

    fix disabling the pagination button

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -11,14 +11,14 @@ const Pagination = ({ page, onPageChange }: Props) => {
     <div className="row d-flex justify-content-center">
       <nav>
         <ul className="pagination">
-          <li className={`page-item ${page.first ? 'disabled' : ''}`} onClick={() => onPageChange(page.number - 1)} >
-            <button className="page-link">Anterior</button>
+          <li className={`page-item ${page.first ? 'disabled' : ''}`}>
+            <button className="page-link" onClick={() => onPageChange(page.number - 1)}>Anterior</button>
           </li>
           <li className="page-item disabled">
             <span className="page-link">{page.number + 1}</span>
           </li>
-          <li className={`page-item ${page.last ? 'disabled' : ''}`} onClick={() => onPageChange(page.number + 1)} >
-            <button className="page-link">Próxima</button>
+          <li className={`page-item ${page.last ? 'disabled' : ''}`}>
+            <button className="page-link" onClick={() => onPageChange(page.number + 1)}>Próxima</button>
           </li>
         </ul>
       </nav>
