4ad96c2f15963f32b4ce9ed34601af84d7863c62
wesleydecezere <wesleydecezere@gmail.com>
2021-10-04 22:02:20 -0300

    Pagination

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
-import Footer from "components/footer";
-import NavBar from "components/navbar";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
 import { Link } from "react-router-dom";
 
 const Home = () => {
